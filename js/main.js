4703ed15d50228f50648d24b42a0a12d5390a9f3
Makarovks320 <kirill.makarov@tafs.pro>
2019-07-02 20:13:55 +0300

    выполнил задание

--- js/main.js
+++ js/main.js
@@ -20,6 +20,12 @@ var adFormNode = document.querySelector('.ad-form');
 var mapFiltersNode = document.querySelector('.map__filters');
 var markerNode = document.querySelector('.map__pin--main');
 var addressInputNode = document.querySelector('#address');
+var housingTypeNode = document.querySelector('#type');
+var priceNode = document.querySelector('#price');
+var timeInNode = document.querySelector('#timein');
+var timeOutNode = document.querySelector('#timeout');
+var roomCountNode = document.querySelector('#room_number');
+var capacityNode = document.querySelector('#capacity');
 
 deactivatePage();
 fillAddress(markerNode);
@@ -30,6 +36,61 @@ markerNode.addEventListener('mouseup', function () {
   fillAddress(markerNode);
 });
 
+
+housingTypeNode.addEventListener('change', function () {
+  updatePrice(priceNode);
+});
+
+timeInNode.addEventListener('change', function () {
+  synchronizeTime(timeOutNode);
+});
+
+timeOutNode.addEventListener('change', function () {
+  synchronizeTime(timeInNode);
+});
+
+roomCountNode.addEventListener('change', function () {
+  enableSelecting(capacityNode);
+  synchronizeCapacity(capacityNode);
+});
+
+
+function synchronizeCapacity(localCapacityNode) {
+  var i;
+  if (event.target.value === '100') {
+    for (i = 0; i < localCapacityNode.children.length - 1; i++) {
+      localCapacityNode.children[i].disabled = true;
+    }
+    localCapacityNode.value = '0';
+  } else {
+    localCapacityNode.lastElementChild.disabled = true;
+    for (i = localCapacityNode.children.length - 2 - event.target.value; i >= 0; i--) {
+      localCapacityNode.children[i].disabled = true;
+    }
+    localCapacityNode.value = event.target.value;
+  }
+}
+
+function enableSelecting(localSelectingNode) {
+  for (var i = 0; i < localSelectingNode.children.length; i++) {
+    localSelectingNode.children[i].disabled = false;
+  }
+}
+
+function synchronizeTime(syncNode) {
+  syncNode.value = event.target.value;
+}
+
+function updatePrice(localPriceNode) {
+  var minPriceList = {
+    bungalo: '0',
+    flat: '1000',
+    house: '5000',
+    palace: '10000'
+  };
+  localPriceNode.placeholder = minPriceList[event.target.value];
+  localPriceNode.min = minPriceList[event.target.value];
+}
 
 function disableForm(form) {
   form.classList.add('ad-form--disabled');
