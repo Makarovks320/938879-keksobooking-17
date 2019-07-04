bc33ba58c801f5fc33ecd389f1727e3bacd48891
Makarovks320 <kirill.makarov@tafs.pro>
2019-07-04 17:48:06 +0300

    убрал код для корректировки количества гостей

--- js/main.js
+++ js/main.js
@@ -24,8 +24,6 @@ var housingTypeNode = document.querySelector('#type');
 var priceNode = document.querySelector('#price');
 var timeInNode = document.querySelector('#timein');
 var timeOutNode = document.querySelector('#timeout');
-var roomCountNode = document.querySelector('#room_number');
-var capacityNode = document.querySelector('#capacity');
 
 deactivatePage();
 fillAddress(markerNode);
@@ -49,33 +47,6 @@ timeOutNode.addEventListener('change', function () {
   synchronizeTime(timeInNode);
 });
 
-roomCountNode.addEventListener('change', function () {
-  enableSelecting(capacityNode);
-  synchronizeCapacity(capacityNode);
-});
-
-
-function synchronizeCapacity(localCapacityNode) {
-  var i;
-  if (event.target.value === '100') {
-    for (i = 0; i < localCapacityNode.children.length - 1; i++) {
-      localCapacityNode.children[i].disabled = true;
-    }
-    localCapacityNode.value = '0';
-  } else {
-    localCapacityNode.lastElementChild.disabled = true;
-    for (i = localCapacityNode.children.length - 2 - event.target.value; i >= 0; i--) {
-      localCapacityNode.children[i].disabled = true;
-    }
-    localCapacityNode.value = event.target.value;
-  }
-}
-
-function enableSelecting(localSelectingNode) {
-  for (var i = 0; i < localSelectingNode.children.length; i++) {
-    localSelectingNode.children[i].disabled = false;
-  }
-}
 
 function synchronizeTime(syncNode) {
   syncNode.value = event.target.value;
