e901f8bfe08886a90dee6907d40ad46ab28ac692
Makarovks320 <kirill.makarov@tafs.pro>
2019-07-11 22:38:47 +0300

    добавил сообщение об ошибке в блоке main, используя блок #error из шаблона template

--- js/load.js
+++ js/load.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/keksobooking/data';
+  var URL = 'https://js.dump.academy/keksobooking/datall';
 
   window.load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
@@ -57,16 +57,10 @@
     };
   };
 
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '30px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
+  var errorHandler = function () {
+    var template = document.querySelector('#error');
+    var node = template.content.querySelector('p').cloneNode(true);
+    document.querySelector('main').insertAdjacentElement('afterbegin', node);
   };
 
   window.load(successHandler, errorHandler);
