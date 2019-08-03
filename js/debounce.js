db4df91a141488b3b2f9986ba8234d44e31f6003
Makarovks320 <kirill.makarov@tafs.pro>
2019-08-04 02:36:06 +0300

    перламутровые пуговицы 2 часть. готово

--- js/debounce.js
+++ js/debounce.js
@@ -3,14 +3,6 @@
   var DEBOUNCE_INTERVAL = 500;
   var lastTimeout;
 
-  // if (lastTimeout) {
-  //   window.clearTimeout(lastTimeout);
-  // }
-  // lastTimeout = window.setTimeout(function () {
-  //   updateInputFilters(evt);
-  //   window.map.updatePins(activeFilters);
-  // }, 500);
-
   window.debounce = function (cb) {
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
